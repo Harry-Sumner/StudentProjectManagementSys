9568b6ebddcdd36085ea208944f5be64bb9821e6
Harry S <Harry S@DESKTOP-CSVSOS0>
2024-04-26 13:46:30 +0100

    Added footer to site. Fixed nav links not working.

---
+++ Project Management System/Pages/Submit_Proposal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_Management_System.Pages
+{
+    public class Submit_ProposalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
