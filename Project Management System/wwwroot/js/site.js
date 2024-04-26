bad259568578ceb38d9a721458912d9cb0a54018
Harry S <Harry S@DESKTOP-CSVSOS0>
2024-04-26 10:19:03 +0100

    Updated layout page with UOC logo and navigation menu. Footer to be completed later.

--- Project Management System/wwwroot/js/site.js
+++ Project Management System/wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    // Assuming '#toggle' is the ID of the button that will be clicked to open and close the sidebar
+    const sidebar = $('#sidebar');
+    const toggle = $('#toggle'); // Single button for toggling the sidebar
+    const divider = $('.header--divider--desktop');
+    const uocLogo = $('.header--uocLogo--standard')
 
-// Write your JavaScript code.
+    toggle.click(function () {
+        // Check if sidebar is open by checking its left style property
+        if (sidebar.css('left') === '0px') {
+            // If it's open, close it
+            sidebar.css('left', '-306px');
+            toggle.css('left', '10px');
+            divider.css('left', '90px');
+            uocLogo.css('left', '110px');
+        } else {
+            // If it's closed, open it
+            sidebar.css('left', '0');
+            toggle.css('left', '316px');
+            divider.css('left', '406px');
+            uocLogo.css('left', '426px');
+        }
+        // Toggle the button's appearance as well
+        toggle.toggleClass('button-open button-close');
+    });
+});
