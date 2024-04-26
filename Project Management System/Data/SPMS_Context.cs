2ccf3fe25d7a6063e5a8b177eb290c9e10edbb40
Harry S <Harry S@DESKTOP-CSVSOS0>
2024-04-26 14:52:02 +0100

    Updated DbContext with Identity. Connected DB.

--- Project Management System/Data/SPMS_Context.cs
+++ Project Management System/Data/SPMS_Context.cs
@@ -9,7 +9,18 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Project_Management_System.Data
 {
-    public class SPMS_Context
+    public class SPMS_Context : IdentityDbContext<SPMS_User>
     {
+        public SPMS_Context(DbContextOptions<SPMS_Context> options) : base(options) { }
+
+        public DbSet<SPMS_User> SPMS_User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<SPMS_User>().Ignore(e => e.Name);
+          
+            
+        }
     }
 }
--- Project Management System/Data/SPMS_User.cs
+++ Project Management System/Data/SPMS_User.cs
@@ -1,6 +1,23 @@
-﻿namespace Project_Management_System.Data
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Project_Management_System.Data
 {
-    public class SPMS_User
+    public class SPMS_User : IdentityUser
     {
+        [PersonalData, Required, StringLength(30)]
+        public string FirstName { get; set; }
+
+        [PersonalData, Required, StringLength(60)]
+        public string Surname { get; set; }
+
+        public string Name { get { return $"{FirstName} {Surname}"; } }
+
+        [PersonalData, StringLength(7)]
+        public string StudentID { get; set; }
+
+        [PersonalData, StringLength(7)]
+        public string StaffID { get; set; }
     }
 }
