3ec5b98ed4b090c7b5ab4b4e3afc12cbe7387d23
Harry S <Harry S@DESKTOP-CSVSOS0>
2024-04-26 14:37:45 +0100

    Installed Nuget Packages

---
+++ Project Management System/Data/SPMS_Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Project_Management_System.Data
+{
+    public class SPMS_Context
+    {
+    }
+}
