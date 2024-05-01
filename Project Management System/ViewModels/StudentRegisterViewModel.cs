dc1dec94c5cd203d8ed9f7c2e049be321df42db5
Harry S <Harry S@DESKTOP-CSVSOS0>
2024-05-01 10:52:08 +0100

    - Merged Login and register into a single page. - Implemented toggle for staff / student forms. - Began to implement staff register.

---
+++ Project Management System/ViewModels/StudentRegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_Management_System.ViewModels
+{
+    public class StudentRegisterViewModel
+    {
+        [Required, DataType(DataType.Text), Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required, DataType(DataType.Text), Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Required, DataType(DataType.Text), Display(Name = "StudentID")]
+        public string StudentID { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
--- Project Management System/wwwroot/js/loginRegisterToggler.js
+++ Project Management System/wwwroot/js/loginRegisterToggler.js
@@ -1,23 +1,59 @@
 $(document).ready(function () {
     $('.body--register--inputToggle').click(function () {
-        if ($('.loginForm').is(':visible')) {
-            // Animate the login form height to 0
-            $('.loginForm').animate({ height: 'toggle' }, 600, function () {
-                // Once animation is complete, show the registration form
-                $('.registerForm').animate({ height: 'toggle' }, 800);
-                $('.body--login--container').css('min-height', '800px');
-                $('.body--login--newUser').text('Already have an account? Click here to login: ');
-                $('.body--register--inputToggle').text('Login');
+        toggleForms();
+    });
+
+    function toggleForms() {
+        var isLoginFormVisible = $('.loginForm').is(':visible');
+        if (isLoginFormVisible) {
+            // Hide login form and show registration form
+            $('.loginForm').slideUp(600, function () {
+                $('.registerForm').slideDown(800);
+                $('.body--register--staffToggler').show(); // Show the toggler for staff/student
+                updateToggleTextAndHeight(true);
             });
         } else {
-            // Animate the registration form height to 0
-            $('.registerForm').animate({ height: 'toggle' }, 800, function () {
-                // Once animation is complete, show the login form
-                $('.loginForm').animate({ height: 'toggle' }, 600);
-                $('.body--login--container').css('min-height', '650px');
-                $('.body--login--newUser').text('New here? Register below: ');
-                $('.body--register--inputToggle').text('Register');
+            // Hide registration form and show login form
+            $('.registerForm').slideUp(800, function () {
+                $('.loginForm').slideDown(600);
+                $('.body--register--staffToggler').hide(); // Hide the toggler
+                updateToggleTextAndHeight(false);
             });
+        }
+    }
+
+    function updateToggleTextAndHeight(isRegisterVisible) {
+        if (isRegisterVisible) {
+            $('.body--login--container').css('min-height', '900px');
+            $('.body--login--newUser').text('Already have an account? Click here to login:');
+            $('.body--register--inputToggle').text('Login');
+        } else {
+            $('.body--login--container').css('min-height', '650px');
+            $('.body--login--newUser').text('New here? Register below:');
+            $('.body--register--inputToggle').text('Register');
+        }
+    }
+
+    // Button events for toggling between student and staff registration forms
+    $('.studentBtn').click(function () {
+        $('.registerFormStaff').hide();
+        $('.registerForm').show();
+    });
+
+    $('.staffBtn').click(function () {
+        $('.registerForm').hide();
+        $('.registerFormStaff').show();
+    });
+    $('#newDepartment').prop('disabled', true);
+
+    // Listen for changes on the school select
+    $('#newSchool').change(function () {
+        var schoolValue = $(this).val();
+        if (schoolValue !== 'null') {
+            $('#newDepartment').prop('disabled', false);
+        } else {
+            $('#newDepartment').prop('disabled', true);
+            $('#newDepartment').val('null');
         }
     });
 });
