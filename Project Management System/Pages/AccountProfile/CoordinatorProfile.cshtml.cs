e287b3aa6c2ff46552ec5771d64e665135f19e17
GEORGE HARRISON <2218541@chester.ac.uk>
2024-05-09 15:57:22 +0100

    Fixed Errors, should run

--- Project Management System/Pages/AccountProfile/CoordinatorProfile.cshtml.cs
+++ Project Management System/Pages/AccountProfile/CoordinatorProfile.cshtml.cs
@@ -8,67 +8,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Project_Management_System.Pages.AccountProfile
 {
+
+    public class CoordinatorProfile : PageModel
     {
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            await LoadAsync(user);
-
-            if (_context.StaffDivision != null)
-            {
-                StaffDivisions = await _context.StaffDivision.ToListAsync();
-            }
-
-            if (_context.Division != null)
-            {
-                Divisions = await _context.Division.ToListAsync();
-            }
-
-            /*if (_context.StaffInterest!= null)
-            {
-                StaffInterest = await _context.StaffInterest.ToListAsync();
-            }*/
-
-            Topic = _db.Topic //select data from database
-                .FromSqlRaw("SELECT * FROM Topic WHERE SupervisorID = {0}", user.Id)
-                .ToList();
-
-            StaffInterest = _db.StaffInterest //select data from database
-                .FromSqlRaw("SELECT * FROM StaffInterest WHERE StaffID = {0}", user.Id)
-                .ToList();
-
-            StaffExpertise = _db.StaffExpertise //select data from database
-                .FromSqlRaw("SELECT * FROM StaffExpertise WHERE StaffID = {0}", user.Id)
-                .ToList();
-
-
-
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync(IFormFile uploadedImage)
-        {
-            if (uploadedImage != null && uploadedImage.Length > 0)
-            {
-                using (var memoryStream = new MemoryStream())
-                {
-                    await uploadedImage.CopyToAsync(memoryStream);
-                    byte[] imageBytes = memoryStream.ToArray();
-
-                    // You can now use the byte array to store the image in your database
-
-                    var user = await _userManager.GetUserAsync(User);
-                    if (user != null)
-                    {
-                        user.ProfilePicture = imageBytes;
-                        await _userManager.UpdateAsync(user);
-                        await _signInManager.RefreshSignInAsync(user);
-                    }
-                }
-            }
-
-            return RedirectToPage();
-        }
+      
     }
 }
