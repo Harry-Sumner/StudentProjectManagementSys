8f5992d4dfdbfd74568aee06f6eb1d9b29613c5d
Harry S <Harry S@DESKTOP-CSVSOS0>
2024-05-13 15:23:24 +0200

    - Made changes to co-ordinator

--- Project Management System/Pages/AccountProfile/CoordinatorProfile.cshtml.cs
+++ Project Management System/Pages/AccountProfile/CoordinatorProfile.cshtml.cs
@@ -9,8 +9,89 @@ using System.ComponentModel.DataAnnotations;
 namespace Project_Management_System.Pages.AccountProfile
 {
 
-    public class CoordinatorProfile : PageModel
+    public class CoordinatorProfileModel : PageModel
     {
-      
+        private readonly UserManager<SPMS_User> _userManager;
+        private readonly SignInManager<SPMS_User> _signInManager;
+        private readonly SPMS_Context _db;
+        private readonly SPMS_Context _context;
+
+
+        public CoordinatorProfileModel(SPMS_Context context,
+            UserManager<SPMS_User> userManager,
+            SignInManager<SPMS_User> signInManager,
+            SPMS_Context db)
+        {
+            _context = context;
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public string Username { get; set; }
+        public IList<Division> Divisions { get; set; } = default!;
+
+        public IList<StaffDivision> StaffDivisions = default!;
+     
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        private async Task LoadAsync(SPMS_User user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+
+            if (_context.StaffDivision != null)
+            {
+                StaffDivisions = await _context.StaffDivision.ToListAsync();
+            }
+
+            if (_context.Division != null)
+            {
+                Divisions = await _context.Division.ToListAsync();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(IFormFile uploadedImage)
+        {
+            if (uploadedImage != null && uploadedImage.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await uploadedImage.CopyToAsync(memoryStream);
+                    byte[] imageBytes = memoryStream.ToArray();
+
+                    // You can now use the byte array to store the image in your database
+
+                    var user = await _userManager.GetUserAsync(User);
+                    if (user != null)
+                    {
+                        user.ProfilePicture = imageBytes;
+                        await _userManager.UpdateAsync(user);
+                        await _signInManager.RefreshSignInAsync(user);
+                    }
+                }
+            }
+
+            return RedirectToPage();
+        }
+
     }
+
+
 }
