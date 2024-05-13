315b8cbbe3ae59da7cdba1314c8299a76a9eb4cd
Scott <2209041@chester.ac.uk>
2024-05-13 14:56:59 +0100

    edited filter functionality and code comments

--- Project Management System/Pages/Index.cshtml.cs
+++ Project Management System/Pages/Index.cshtml.cs
@@ -9,7 +9,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 namespace Project_Management_System.Pages
 {
     [Authorize]
@@ -21,6 +20,7 @@ namespace Project_Management_System.Pages
         private readonly UserManager<SPMS_Student> _userManager;
         private readonly SignInManager<SPMS_User> _signInManager;
 
+        // Constructor for the IndexModel
         public IndexModel(ILogger<IndexModel> logger, SPMS_Context context, SPMS_Context db, UserManager<SPMS_Student> userManager, SignInManager<SPMS_User> signInManager)
         {
             _logger = logger;
@@ -30,42 +30,51 @@ namespace Project_Management_System.Pages
             _signInManager = signInManager;
         }
 
+        // Properties to hold topics, courses, and search query
         public IList<Topic> Topic { get; set; } = new List<Topic>();
         public IList<Course> Course { get; set; } = new List<Course>();
-        // Bind the search query parameter from the URL
         public string Search { get; set; }
 
-        public async Task OnGetAsync(string search)
+        // Handler for HTTP GET requests
+        public async Task OnGetAsync(string search, string course)
         {
-            // If a search query is provided, filter topics
-            if (!string.IsNullOrEmpty(search))
+            // If course is provided, filter courses by course name
+            if (!string.IsNullOrEmpty(course))
+            {
+                Course = await _context.Course
+                    .Where(t => t.CourseName == course)
+                    .ToListAsync();
+            }
+            // If search query is provided, filter topics by topic name
+            else if (!string.IsNullOrEmpty(search))
             {
                 Topic = await _context.Topic
                     .Where(t => EF.Functions.Like(t.TopicName, $"%{search}%"))
                     .ToListAsync();
             }
+            // If neither course nor search query is provided, retrieve all topics
             else
             {
-                // If no search query is provided, retrieve all topics
                 Topic = await _context.Topic.ToListAsync();
             }
 
-            if(_context.Course != null)
-            {
-                Course = await _context.Course.ToListAsync();
-            }
+            // Retrieve all courses
+            Course = await _context.Course.ToListAsync();
         }
 
+        // Handler for adding a topic to the user's basket
         public async Task<IActionResult> OnPostAddAsync(int topicID)
         {
             var user = await _userManager.GetUserAsync(User);
 
+            // Check if the topic is already added to the user's basket
             var topics = await _db.TopicBasket
                 .FromSqlRaw("SELECT * FROM TopicBasket WHERE TopicID = {0} AND StudentID = {1}", topicID, user.Id)
                 .ToListAsync();
 
             if (topics.Count == 0)
             {
+                // If the topic is not already added, add it to the basket
                 TopicBasket newTopic = new TopicBasket
                 {
                     StudentID = user.Id,
@@ -78,17 +87,25 @@ namespace Project_Management_System.Pages
             }
             else
             {
-                // This handles the  case where  the topic has already been added
+                // If the topic is already added, show a message
                 TempData["ShowPopup"] = "Topic is already added to your proposal.";
             }
 
             return RedirectToPage();
         }
 
+        // Handler for HTTP POST requests to perform a search
         public async Task<IActionResult> OnPostSearchAsync()
         {
-            await OnGetAsync(Search);
+            // Call the OnGetAsync method with search query and course parameter
+            await OnGetAsync(Search, Course);
             return Page();
+        }
+
+        // Private method to handle HTTP GET requests with additional parameters
+        private async Task OnGetAsync(string search, IList<Course> course)
+        {
+            throw new NotImplementedException();
         }
     }
 }
